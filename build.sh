98f87be448e14066d67a84490bbba434cccda4b1
Artur Morelle Nista <arturnista@gmail.com>
2024-01-28 16:51:20 -0300

    Removing builds

--- build.sh
+++ build.sh
@@ -2,15 +2,16 @@ echo Building...
 dotnet publish -c Release
 # Clear build files
 echo Clearing old files...
-rm -rf Web.zip
-rm -rf Windows.zip
+mkdir Build/
+rm -rf Build/Web.zip
+rm -rf Build/Windows.zip
 rm -rf ./Source/bin/Release/net7.0/publish/resources/
 # Clear copy resources
 echo Copyin resources...
 cp -r ./Source/resources/ ./Source/bin/Release/net7.0/publish/resources/
 # Create zips
 echo Creating zips...
-Tar -a -cf Web.zip DotnetRaylibWasm/bin/Release/net7.0/browser-wasm/AppBundle/
+Tar -a -cf Build/Web.zip DotnetRaylibWasm/bin/Release/net7.0/browser-wasm/AppBundle/
 cp -r Source/bin/Release/net7.0/publish/ ./WindowsGame
-Tar -a -cf Windows.zip WindowsGame/
+Tar -a -cf Build/Windows.zip WindowsGame/
 rm -rf WindowsGame
