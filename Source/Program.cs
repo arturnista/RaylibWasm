69aaca7f37c8f37dcfe4ca8fb2a57b6a960af77f
Artur Morelle Nista <arturnista@gmail.com>
2024-10-17 12:10:31 -0300

    Minnor code changes

--- Source/Program.cs
+++ Source/Program.cs
@@ -6,6 +6,9 @@ namespace Source;
 public class Program
 {
 
+    private const int WINDOW_WIDTH = 1124;
+    private const int WINDOW_HEIGHT = 740;
+
     public class Orc
     {
         public Vector2 Position;
@@ -14,7 +17,8 @@ public class Program
 
     public static int Main()
     {
-        Raylib.InitWindow(800, 480, "Hello World");
+        int worldMargin = 16;
+        Raylib.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello World");
         Texture2D orcTexture = Raylib.LoadTexture("resources/orc.png");
         
         List<Orc> allOrcs = new List<Orc>();
@@ -22,39 +26,104 @@ public class Program
         while (!Raylib.WindowShouldClose())
         {
             float deltaTime = Raylib.GetFrameTime();
-            if (Raylib.IsMouseButtonPressed(0))
+            if (Raylib.IsMouseButtonPressed(MouseButton.Left))
             {
                 var position = Raylib.GetMousePosition();
                 allOrcs.Add(new Orc()
                 {
-                    Position = new Vector2(position.X, 480 - position.Y),
+                    Position = new Vector2(position.X, WINDOW_HEIGHT - position.Y),
                     Velocity = new Vector2(
                         Random.Shared.NextSingle() * 500f - 250f,
                         Random.Shared.NextSingle() * 500f - 250f
                     )
                 });
             }
+            if (Raylib.IsMouseButtonDown(MouseButton.Right))
+            {
+                var position = Raylib.GetMousePosition();
+                var fixedPosition = new Vector2(position.X, WINDOW_HEIGHT - position.Y);
+                foreach (var orc in allOrcs)
+                {
+                    var direction = Vector2.Normalize(fixedPosition - orc.Position);
+                    orc.Velocity += direction * 500f * deltaTime;
+                }
+            }
+
+            if (Raylib.IsKeyPressed(KeyboardKey.Space))
+            {
+                allOrcs.Clear();
+            }
+
+            Vector2 boost = Vector2.Zero;
+            int force = 500;
+            if (Raylib.IsKeyPressed(KeyboardKey.Left))
+            {
+                boost = new Vector2(-force, 0f);
+            }
+            else if (Raylib.IsKeyPressed(KeyboardKey.Right))
+            {
+                boost = new Vector2(force, 0f);
+            }
+            else if (Raylib.IsKeyPressed(KeyboardKey.Up))
+            {
+                boost = new Vector2(0f, force);
+            }
+            else if (Raylib.IsKeyPressed(KeyboardKey.Down))
+            {
+                boost = new Vector2(0f, -force);
+            }
 
             foreach (var orc in allOrcs)
             {
+                orc.Velocity += boost;
+                if (orc.Velocity.Length() > 500)
+                {
+                    orc.Velocity = Vector2.Normalize(orc.Velocity) * 500;
+                }
+
                 orc.Position += orc.Velocity * deltaTime;
-                orc.Velocity += new Vector2(0f, -50f) * deltaTime;
-                if (orc.Position.Y < 0f) orc.Velocity = Vector2.Reflect(orc.Velocity, Vector2.UnitY) * (Random.Shared.NextSingle() * 1f + 0.5f);
-                else if (orc.Position.Y > 480f) orc.Velocity = Vector2.Reflect(orc.Velocity, -Vector2.UnitY) * (Random.Shared.NextSingle() * 1f + 0.5f);
-                else if (orc.Position.X < 0f) orc.Velocity = Vector2.Reflect(orc.Velocity, Vector2.UnitX) * (Random.Shared.NextSingle() * 1f + 0.5f);
-                else if (orc.Position.X > 800f) orc.Velocity = Vector2.Reflect(orc.Velocity, -Vector2.UnitX) * (Random.Shared.NextSingle() * 1f + 0.5f);
+                
+                if (orc.Position.Y < worldMargin)
+                {
+                    orc.Position.Y = WINDOW_HEIGHT - 300;
+                }
+                else if (orc.Position.Y > WINDOW_HEIGHT - 300)
+                {
+                    orc.Position.Y = worldMargin;
+                }
+                else if (orc.Position.X < worldMargin)
+                {
+                    orc.Position.X = WINDOW_WIDTH - worldMargin;
+                }
+                else if (orc.Position.X > WINDOW_WIDTH - worldMargin)
+                {
+                    orc.Position.X = worldMargin;
+                }
             }
 
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
 
-            Raylib.DrawText($"Press to create orcs", 12, 25, 20, Color.Black);
+            var xMin = worldMargin;
+            var xMax = WINDOW_WIDTH - worldMargin;
+            var yMin = WINDOW_HEIGHT - worldMargin;
+            var yMax = 300;
+
+            Raylib.DrawLine(xMin, yMin, xMin, yMax, Color.Black);
+            Raylib.DrawLine(xMax, yMin, xMax, yMax, Color.Black);
+
+            Raylib.DrawLine(xMin, yMin, xMax, yMin, Color.Black);
+            Raylib.DrawLine(xMin, yMax, xMax, yMax, Color.Black);
+
+            Raylib.DrawText($"Left Click to create orcs", 12, 25, 20, Color.Black);
+            Raylib.DrawText($"Right Click to pull orcs", 312, 25, 20, Color.Black);
+            Raylib.DrawText($"Space to clear all orcs", 612, 25, 20, Color.Black);
             Raylib.DrawText($"Orcs created: {allOrcs.Count}", 12, 45, 20, Color.Black);
             foreach (var orc in allOrcs)
             {
-                Raylib.DrawTexture(orcTexture, (int)orc.Position.X, 480 - (int)orc.Position.Y, Color.White);
+                Raylib.DrawTexture(orcTexture, (int)orc.Position.X, WINDOW_HEIGHT - (int)orc.Position.Y, Color.White);
             }
-            Raylib.DrawFPS(12, 450);
+            Raylib.DrawFPS(12, WINDOW_HEIGHT - 25);
             Raylib.EndDrawing();
         }
         Raylib.UnloadTexture(orcTexture);
--- build.sh
+++ build.sh
@@ -2,16 +2,17 @@ echo Building...
 dotnet publish -c Release
 # Clear build files
 echo Clearing old files...
+
 mkdir Build/
-rm -rf Build/Web.zip
-rm -rf Build/Windows.zip
+rm -rf Build/Web
+rm -rf Build/Windows
 rm -rf ./Source/bin/Release/net7.0/publish/resources/
+
 # Clear copy resources
 echo Copyin resources...
 cp -r ./Source/resources/ ./Source/bin/Release/net7.0/publish/resources/
 # Create zips
 echo Creating zips...
-Tar -a -cf Build/Web.zip DotnetRaylibWasm/bin/Release/net7.0/browser-wasm/AppBundle/
-cp -r Source/bin/Release/net7.0/publish/ ./WindowsGame
-Tar -a -cf Build/Windows.zip WindowsGame/
-rm -rf WindowsGame
+
+cp -r DotnetRaylibWasm/bin/Release/net7.0/browser-wasm/AppBundle/ ./Build/Web
+cp -r Source/bin/Release/net7.0/publish/ ./Build/Windows
